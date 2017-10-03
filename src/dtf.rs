7584744037367cb45706fb2721f6ff72aa96f5d6
Ricky Han <rickylqhan@gmail.com>
2017-10-03 00:32:16 -0400

    small refactor

--- src/dtf.rs
+++ src/dtf.rs
@@ -3,20 +3,20 @@
 // File Type:
 // Offset 00: ([u8; 5]) magic value 0x4454469001
 // Offset 05: ([u8; 8]) Symbol
-// Offset 13: (u64) how many records
+// Offset 13: (u64) number of records
 // Offset 21: (u32) max ts
-// Offset 80: -- records(see below) --
+// Offset 80: -- records - see below --
 
 // Record Spec:
 // Offset 81: bool for is_snapshot
-// 1. if is_snapshot
+// 1. if is snapshot
 //        4 bytes (u32): reference ts
 //        2 bytes (u16): reference seq
 //        2 bytes (u16): how many records between this snapshot and the next snapshot
 //        
-// 2. if ! is_snapshot
-//        1 byte (u8): ts - reference ts
-//        1 byte (u8): seq - reference seq 
+// 2. if is record
+//        dts (u8): $ts - reference ts$
+//        dseq (u8): $seq - reference seq$ 
 //        is_trade: (u8):
 //        is_bid: (u8)
 //        price: (f32)
@@ -54,6 +54,7 @@ impl Ord for Update {
         return self.partial_cmp(&other).unwrap();
     }
 }
+
 impl Eq for Update {}
 
 impl PartialOrd for Update {
@@ -90,32 +91,55 @@ pub fn serialize_update(update : &Update) -> Vec<u8> {
     buf
 }
 
+pub fn get_max_ts(updates : &Vec<Update>) -> u64 {
+    let mut max = 0;
+    for update in updates.iter() {
+        let current = update.ts;
+        if current > max {
+            max = current;
+        }
+    }
+    max
+}
 
-pub fn encode(fname : &String, symbol : &String, ts : &Vec<Update>) {
+pub fn encode(fname : &String, symbol : &String, ups : &Vec<Update>) {
     let new_file = File::create(fname).unwrap();
     let mut writer = BufWriter::new(new_file);
     // headers
     writer.write(MAGIC_VALUE).unwrap();
 
+    // write symbol
     assert!(symbol.len() <= 8);
     let padded_symbol = format!("{:8}", symbol);
     assert!(padded_symbol.len() == 8);
-
-    // write symbol
     writer.write(padded_symbol.as_bytes()).unwrap();
 
-    // how many records
-    writer.write_u64::<BigEndian>(ts.len() as u64).expect("length of records");
+    // number of records
+    writer.write_u64::<BigEndian>(ups.len() as u64).expect("length of records");
+
+    // max ts
+    let max_ts = get_max_ts(&ups);
+    writer.write_u64::<BigEndian>(max_ts).expect("maximum timestamp");
 
     writer.seek(SeekFrom::Start(MAIN_OFFSET)).unwrap();
-    for elem in ts.into_iter() {
+    for elem in ups.into_iter() {
         let serialized = serialize_update(&elem);
         writer.write(serialized.as_slice()).unwrap();
     }
     writer.flush().expect("FAILURE TO FLUSH");
 }
 
-pub fn get_symbol(fname: &String) -> String {
+//TODO:
+pub fn append(fname: &String, ups : &Vec<Update>) {
+    let mut rdr = file_reader(&fname);
+    let symbol = read_symbol(&mut rdr);
+
+}
+
+// parsers:
+
+fn file_reader(fname: &String) -> BufReader<File> {
+
     let file = File::open(fname).expect("OPENING FILE");
     let mut rdr = BufReader::new(file);
 
@@ -125,6 +149,11 @@ pub fn get_symbol(fname: &String) -> String {
     if buf != MAGIC_VALUE {
         panic!("MAGIC VALUE INCORRECT");
     }
+
+    rdr 
+}
+pub fn read_symbol(rdr : &mut BufReader<File>) -> String {
+    rdr.seek(SeekFrom::Start(5));
 
     // read symbol
     let mut buffer = [0; 8];
@@ -134,18 +163,17 @@ pub fn get_symbol(fname: &String) -> String {
     symbol
 }
 
-pub fn read_first(fname: &String) -> Update {
-    let file = File::open(fname).expect("OPENING FILE");
-    let mut rdr = BufReader::new(file);
+pub fn read_len(rdr : &mut BufReader<File>) -> u64 {
+    rdr.seek(SeekFrom::Start(13));
+    rdr.read_u64::<BigEndian>().expect("length of records")
+}
 
-    // magic value
-    let mut buf = vec![0u8; 5];
-    rdr.read_exact(&mut buf).unwrap();
-    if buf != MAGIC_VALUE {
-        panic!("MAGIC VALUE INCORRECT");
-    }
+pub fn read_max_ts(rdr : &mut BufReader<File>) -> u64 {
+    rdr.seek(SeekFrom::Start(21));
+    rdr.read_u64::<BigEndian>().expect("maximum timestamp")
+}
 
-    rdr.seek(SeekFrom::Start(MAIN_OFFSET)).expect("SEEKING");
+pub fn read_one(rdr: &mut BufReader<File>) -> Update {
 
     let current_update = Update {
         ts: rdr.read_u64::<BigEndian>().expect("ts"),
@@ -159,39 +187,20 @@ pub fn read_first(fname: &String) -> Update {
     current_update
 }
 
+pub fn read_first(mut rdr: &mut BufReader<File>) -> Update {
+    rdr.seek(SeekFrom::Start(MAIN_OFFSET)).expect("SEEKING");
+    read_one(&mut rdr)
+}
+
 pub fn decode(fname: &String) -> Vec<Update> {
     let mut v : Vec<Update> = Vec::new();
-    let file = File::open(fname).expect("OPENING FILE");
-    let mut rdr = BufReader::new(file);
-
-    // magic value
-    let mut buf = vec![0u8; 5];
-    rdr.read_exact(&mut buf).unwrap();
-    if buf != MAGIC_VALUE {
-        panic!("MAGIC VALUE INCORRECT");
-    }
-
-    // read symbol
-    let mut buffer = [0; 8];
-    rdr.read_exact(&mut buffer).unwrap();
-    let _ = str::from_utf8(&buffer).unwrap();
-    // println!("{}", symbol);
-
-    // read length
-
-    let nums = rdr.read_u64::<BigEndian>().expect("length of records");
-
+    let mut rdr = file_reader(&fname);
+    let _symbol = read_symbol(&mut rdr); 
+    let nums = read_len(&mut rdr);
+    let _max_ts = read_max_ts(&mut rdr);
     for n in 0..nums {
         rdr.seek(SeekFrom::Start(MAIN_OFFSET + n * ITEM_OFFSET)).expect("SEEKING");
-        let current_update = Update {
-            ts: rdr.read_u64::<BigEndian>().expect("ts"),
-            seq: rdr.read_u32::<BigEndian>().expect("seq"),
-            is_trade: rdr.read_u8().expect("is_trade") == 0x00000001,
-            is_bid: rdr.read_u8().expect("is_bid") == 0x00000001,
-            price: rdr.read_f32::<BigEndian>().expect("price"),
-            size: rdr.read_f32::<BigEndian>().expect("size")
-        };
-        v.push(current_update);
+        v.push(read_one(&mut rdr));
     }
 
     v
@@ -241,7 +250,8 @@ fn should_encode_and_decode_file() {
 fn should_return_correct_symbol() {
     init();
     let fname = "test.bin".to_owned();
-    let sym = get_symbol(&fname);
+    let mut rdr = file_reader(&fname);
+    let sym = read_symbol(&mut rdr);
     assert_eq!(sym, "NEO_BTC ");
 }
 
@@ -249,6 +259,25 @@ fn should_return_correct_symbol() {
 fn should_return_first_record() {
     let vs = init();
     let fname = "test.bin".to_owned();
-    let v = read_first(&fname);
+    let mut rdr = file_reader(&fname);
+    let v = read_first(&mut rdr);
     assert_eq!(vs[0], v);
+}
+
+#[test]
+fn should_return_correct_num_of_items() {
+    let vs = init();
+    let fname = "test.bin".to_owned();
+    let mut rdr = file_reader(&fname);
+    let len = read_len(&mut rdr);
+    assert_eq!(vs.len() as u64, len);
+}
+
+#[test]
+fn should_return_max_ts() {
+    let vs = init();
+    let fname = "test.bin".to_owned();
+    let mut rdr = file_reader(&fname);
+    let max_ts = read_max_ts(&mut rdr);
+    assert_eq!(max_ts, get_max_ts(&vs));
 }
