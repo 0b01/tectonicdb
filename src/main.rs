59f3ac7a606902408829ba1534d40167aecfbfba
ricky han <rickylqhan@gmail.com>
2017-10-03 17:40:43 -0400

    simple tcp echo server

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,30 @@
 extern crate byteorder;
+
 mod db;
-use db::*;
-
 mod conf;
-use conf::get_config;
-
 mod dtf;
-use dtf::*;
-
 mod server;
+
+use db::*;
+use conf::get_config;
+use dtf::*;
 use server::*;
 
-use std::thread;
-use std::net::{TcpListener, TcpStream};
-use std::io::{BufReader, BufWriter, Write, BufRead};
 
 
 fn main() {
-    let conf = get_config();
-    let cxn_str : &String = &conf["connection_string"];
+    // let conf = get_config();
+    // let cxn_str : &String = &conf["connection_string"];
+    // let updates : Vec<OrderBookUpdate> = db::run(&cxn_str);
+    // let mut mapped : Vec<Update> = updates.iter().map(|d| d.to_update()).collect();
+    // mapped.sort(); // important
+    // // println!("{:?}", mapped);
+    // let fname = "real.bin".to_owned();
+    // let symbol = "NEO_BTC".to_owned();
+    // encode(&fname, &symbol, &mut mapped);
+
+    
+    run_server();
 
 
-    let updates : Vec<OrderBookUpdate> = db::run(&cxn_str);
-    let mut mapped : Vec<Update> = updates.iter().map(|d| d.to_update()).collect();
-
-    println!("{:?}", mapped);
-
-    let fname = "real.bin".to_owned();
-    let symbol = "NEO_BTC".to_owned();
-    encode(&fname, &symbol, &mut mapped);
-
-
-
-    // // server
-    // thread::spawn(start_server);
-
-    // let player_stream = TcpStream::connect("127.0.0.1:8000").expect("Couldn't connect");
-
-    // let mut reader = BufReader::new(&player_stream);
-    // let mut response = String::new();
-    // reader.read_line(&mut response).expect("Could not read");
-    // println!("Player received >{}<", response.trim());
-
-    // let mut writer = BufWriter::new(&player_stream);
-    // writer.write_all("NAME\n".as_bytes()).expect("Could not write");
 }
--- src/server.rs
+++ src/server.rs
@@ -1,25 +1,46 @@
-use std::net::{TcpListener, TcpStream};
-use std::io::{BufReader, BufWriter, Write, BufRead};
+use std::net::TcpStream;
+use std::net::TcpListener;
+use std::thread;
 
+// traits
+use std::io::{Read, Write};
 
-pub fn start_server() {
-    let listener = TcpListener::bind("127.0.0.1:8000").unwrap();
+pub fn handle_client(mut stream: TcpStream) {
+    let mut buf;
+    loop {
+        // clear out the buffer so we don't send garbage
+        buf = [0; 512];
+        let _ = match stream.read(&mut buf) {
+            Err(e) => panic!("Got an error: {}", e),
+            Ok(m) => {
+                if m == 0 {
+                    // we've got an EOF
+                    break;
+                }
+                m
+            },
+        };
 
-    fn handle_client(stream: TcpStream) {
-        println!("Client connected");
+        println!("GOT: {:?}", "1");
 
-        let mut writer = BufWriter::new(&stream);
-        writer.write_all("Red\n".as_bytes()).expect("could not write");
-        writer.flush().expect("could not flush");
-
-        let mut reader = BufReader::new(&stream);
-        let mut response = String::new();
-        reader.read_line(&mut response).expect("could not read");
-        println!("Server received {}", response);
+        match stream.write(&buf) {
+            Err(_) => break,
+            Ok(_) => continue,
+        }
     }
+}
 
+
+pub fn run_server(){
+    let listener = TcpListener::bind("127.0.0.1:9001").unwrap();
     for stream in listener.incoming() {
-        let stream = stream.expect("Unable to accept");
-        handle_client(stream);
+        match stream {
+            Err(e) => { println!("failed: {}", e) }
+            Ok(stream) => {
+                thread::spawn(move || {
+                    handle_client(stream)
+                });
+            }
+        }
     }
 }
