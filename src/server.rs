89a598c52129303e6a13e8365fbcfc5e69332b3e
ricky han <rickylqhan@gmail.com>
2017-10-04 10:43:30 -0400

    added exception handling

--- src/server.rs
+++ src/server.rs
@@ -31,9 +31,9 @@ struct State {
     v: Vec<dtf::Update>
 }
 
-fn parse_line(string : &str) -> dtf::Update {
+fn parse_line(string : &str) -> Option<dtf::Update> {
 
-    let mut u = dtf::Update { ts : 0, seq : 0, is_bid : false, is_trade : false, price : 0., size : 0.};
+    let mut u = dtf::Update { ts : 0, seq : 0, is_bid : false, is_trade : false, price : -0.1, size : -0.1 };
     let mut buf : String = String::new();
     let mut count = 0;
     let mut most_current_bool = false;
@@ -51,12 +51,12 @@ fn parse_line(string : &str) -> dtf::Update {
         } else if ch == ',' || ch == ';' {
             println!("{}", buf);
             match count {
-                0 => { u.ts = buf.parse::<u64>().unwrap(); },
-                1 => { u.seq = buf.parse::<u32>().unwrap(); },
+                0 => { u.ts       = match buf.parse::<u64>() {Ok(ts) => ts, Err(err) => return None}},
+                1 => { u.seq      = match buf.parse::<u32>() {Ok(seq) => seq, Err(err) => return None}},
                 2 => { u.is_trade = most_current_bool; },
-                3 => { u.is_bid = most_current_bool; },
-                4 => { u.price = buf.parse::<f32>().unwrap(); },
-                5 => { u.size = buf.parse::<f32>().unwrap(); },
+                3 => { u.is_bid   = most_current_bool; },
+                4 => { u.price    = match buf.parse::<f32>() {Ok(price) => price, Err(err) => return None} },
+                5 => { u.size     = match buf.parse::<f32>() {Ok(size) => size, Err(err) => return None}},
                 _ => panic!("IMPOSSIBLE")
             }
             count += 1;
@@ -64,7 +64,21 @@ fn parse_line(string : &str) -> dtf::Update {
         }
     }
 
-    u
+    Some(u)
+}
+
+#[test]
+fn should_parse_string_not_okay() {
+    let string = "1505177459.658, 139010,,, f, t, 0.0703629, 7.65064249;";
+    let target = dtf::Update {
+        ts: 1505177459658,
+        seq: 139010,
+        is_trade: false,
+        is_bid: true,
+        price: 0.0703629,
+        size: 7.65064249
+    };
+    assert!(parse_line(&string).is_none());
 }
 
 #[test]
@@ -78,7 +92,7 @@ fn should_parse_string_okay() {
         price: 0.0703629,
         size: 7.65064249
     };
-    assert_eq!(target, parse_line(&string));
+    assert_eq!(target, parse_line(&string).unwrap());
 
     let string1 = "1505177459.650, 139010, t, f, 0.0703620, 7.65064240;";
     let target1 = dtf::Update {
@@ -89,48 +103,52 @@ fn should_parse_string_okay() {
         price: 0.0703620,
         size: 7.65064240
     };
-    assert_eq!(target1, parse_line(&string1));
+    assert_eq!(target1, parse_line(&string1).unwrap());
 }
 
 
-fn gen_response(string : &str, state: &mut State) -> String {
+fn gen_response(string : &str, state: &mut State) -> Option<String> {
 
     match string {
-        "" => "".to_owned(),
-        "PING" => "PONG.\n".to_owned(),
-        "INFO" => format!("DB: {}", state.db),
+        "" => Some("".to_owned()),
+        "PING" => Some("PONG.\n".to_owned()),
+        "INFO" => Some(format!("DB: {}", state.db)),
         _ => {
             if state.is_adding {
-                state.v.push(parse_line(string));
-                "".to_owned()
+                let parsed = parse_line(string);
+                match parsed {
+                    Some(up) => state.v.push(up),
+                    None => return None
+                }
+                Some("".to_owned())
             } else
 
             if string.starts_with("DB") {
                 let dbname : &str = &string[3..];
                 state.db = dbname.to_owned();
-                format!("SWITCHED TO DB `{}`.\n", &dbname)
+                Some(format!("SWITCHED TO DB `{}`.\n", &dbname))
             } else
 
             if string.starts_with("ADD") {
-                "".to_owned()
+                Some("".to_owned())
             } else 
 
             if string.starts_with("GET") {
-                "".to_owned()
+                Some("".to_owned())
             } else
 
             if string.starts_with("BULKADD") {
                 state.is_adding = true;
-                "".to_owned()
+                Some("".to_owned())
             } else
 
             if string.starts_with("DDAKLUB") {
                 state.is_adding = false;
-                "".to_owned()
+                Some("".to_owned())
             }
 
             else {
-                format!("-ERR unknown command '{}'.\n", &string)
+                Some(format!("-ERR unknown command '{}'.\n", &string))
             }
         }
     }
@@ -151,7 +169,10 @@ fn handle_client(mut stream: TcpStream) {
         let ping = str::from_utf8(&buf[..(bytes_read-1)]).unwrap();
 
         let resp = gen_response(&ping, &mut state);
-        stream.write(resp.as_bytes()).unwrap();
+        match resp {
+            Some(str_resp) => stream.write(str_resp.as_bytes()).unwrap(),
+            None => stream.write("ERROR".as_bytes()).unwrap()
+        };
     }
 }
 
