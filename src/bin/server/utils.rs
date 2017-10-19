741df7cf5cce716137288b60735e511ba67f9438
ricky han <rickylqhan@gmail.com>
2017-10-19 13:03:12 -0400

    fixed bug

--- src/bin/server/utils.rs
+++ src/bin/server/utils.rs
@@ -13,12 +13,24 @@ pub fn create_dir_if_not_exist(dtf_folder : &str) {
 /// Create corresponding Store objects in State.
 pub fn init_dbs(dtf_folder : &str, state: &mut State) {
     for dtf_file in fs::read_dir(&dtf_folder).unwrap() {
-        let dtf_file = dtf_file.unwrap();
-        let fname_os = dtf_file.file_name();
-        let fname = fname_os.to_str().unwrap();
+        let fname_os = dtf_file.unwrap().file_name();
+        let fname = fname_os.to_str().unwrap(); // something.dtf
         if fname.ends_with(".dtf") {
-            let name = Path::new(&fname_os).file_stem().unwrap().to_str().unwrap();
-            let header_size = dtf::get_size(&format!("{}/{}", dtf_folder, fname));
+            let name = Path::new(&fname_os)
+                       .file_stem() // (.*).dtf
+                       .unwrap()
+                       .to_str()
+                       .unwrap();
+            let full_path = &format!("{}/{}", dtf_folder, fname);
+            let header_size = dtf::get_size(full_path);
+
+            // insert a vector into shared hashmap
+            {
+                let mut global = state.global.write().unwrap();
+                global.vec_store.insert(name.to_owned(), Vec::new());
+            }
+
+            // insert a db store into user state
             state.store.insert(name.to_owned(), Store {
                 name: name.to_owned(),
                 size: header_size,
