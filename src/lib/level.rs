4366905c5e7c7ee6320e9649b331c69bccb34ce4
Ricky <rickylqhan@gmail.com>
2017-11-16 23:29:54 +0000

    fix: implement levels

--- src/lib/level.rs
+++ src/lib/level.rs
@@ -9,9 +9,10 @@ use super::utils::price_histogram::{Histogram, Count};
 type Price = f32;
 type Size = f32;
 type Time = u32;
+type PriceBits = u64;
 
 struct Levels {
-    levels: HashMap<Price, BTreeMap<Time, Size>>
+    levels: HashMap<PriceBits, BTreeMap<Time, Size>>
 }
 
 impl Levels {
@@ -20,24 +21,38 @@ impl Levels {
     }
 
     fn from(ups: &[super::Update], step_bins: Count, tick_bins: Count) -> Levels {
+        // build price histogram
         let prices = ups.iter().map(|up| up.price as f64).collect::<Vec<f64>>();
         let price_hist = Histogram::new(&prices, tick_bins);
 
+        // build time step histogram
         let min_ts = ups.iter().next().unwrap().ts / 1000;
         let max_ts = ups.iter().next_back().unwrap().ts / 1000;
+        let bucket_size = (max_ts - min_ts) / (step_bins as u64);
+        let mut boundaries = vec![];
+        for i in 0..(step_bins+1) {
+            boundaries.push((min_ts + i as u64) as f64 * bucket_size as f64);
+        }
+        let step_hist = Histogram { bins: None, boundaries };
 
-        println!("min {} ::::: max {}", min_ts, max_ts);
+        // build map for levels
+        let mut map = HashMap::new();
+        for up in ups.iter() {
+            let price = price_hist.to_bin(up.price as f64);
+            let time = step_hist.to_bin((up.ts / 1000) as f64);
+            match (price, time) {
+                (None, _) => { continue; },
+                (_, None) => { continue; },
+                (Some(p), Some(t)) => {
+                    let price_level = map.entry(p.to_bits()).or_insert(BTreeMap::<Time, Size>::new());
+                    (*price_level).insert(t as Time, up.size);
+                }
+            }
+        };
 
-        let range = (min_ts..max_ts).map(|i| i as f64)
-                                    .collect::<Vec<f64>>();
-        let time_hist = Histogram::new(range.as_slice(), step_bins);
-        println!("{:?}", time_hist.boundaries);
-
-        unimplemented!();
-
-        // Levels {
-        //     levels
-        // }
+        Levels {
+            levels: map
+        }
     }
 }
 
@@ -53,17 +68,7 @@ mod tests {
     }
 }
 
-// def to_updates(events):
-//     tick_bins_cnt = 2000
-//     step_bins_cnt = 2000
-//     min_ts = result[0][0]
-//     max_ts = result[-1][0]
-//     step_thresholds = range(int(floor(min_ts)), int(ceil(max_ts)), int(floor((max_ts - min_ts)/(step_bins_cnt))))
-//     def into_step_bin(time):
-//         for (s, b) in zip(step_thresholds, step_thresholds[1:]):
-//             if b > time > s:
-//                 return b
-//         return False
+// def to_levels(events):
 //     updates = {}
 //     for row in result:
 //         ts, seq, size, price, is_bid, is_trade = row
