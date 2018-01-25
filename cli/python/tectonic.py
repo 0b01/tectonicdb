b14e5a2de87e2cf0d85e908d8cc331bf173f334d
Ricky Han <rickylqhan@gmail.com>
2018-01-25 00:58:58 -0500

    refactor: modify tectonic.py to support 3.6 async generator

--- cli/python/tectonic.py
+++ cli/python/tectonic.py
@@ -7,120 +7,211 @@ import json
 import struct
 import time
 import sys
+import asyncio
 
 class TectonicDB():
-    def __init__(self, host="localhost", port=9001):
+    """
+    Example Usage:
+        from tectonic import TectonicDB
+        import json
+        import asyncio
 
+        async def subscribe(name):
+            db = TectonicDB()
+            print(await db.subscribe(name))
+            while 1:
+                _, item = await db.poll()
+                if item == b"NONE":
+                    await asyncio.sleep(0.01)
+                else:
+                    yield json.loads(item)
+
+        class TickBatcher(object):
+            def __init__(self, db_name):
+                self.one_batch = []
+                self.db_name = db_name
+
+            async def batch(self):
+                generator = subscribe(self.db_name)
+                async for item in generator:
+                    self.one_batch.append(item)
+            
+            async def timer(self):
+                while 1:
+                    await asyncio.sleep(5)
+                    print(len(self.one_batch))
+                
+            
+        if __name__ == '__main__':
+            loop = asyncio.get_event_loop()
+            proc = TickBatcher("bnc_xrp_btc")
+            loop.create_task(proc.batch())
+            loop.create_task(proc.timer())
+            loop.run_forever()
+            loop.close()
+    """
+    def __init__(self, host="localhost", port=9001):
         self.subscribed = False
+        self.host = host
+        self.port = port
 
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         server_address = (host, port)
         self.sock.connect(server_address)
 
-    def cmd(self, command):
-        if type(command) != str:
-            message = (command.decode() + '\n').encode()
-        else:
-            message = (command+'\n').encode()
-        self.sock.sendall(message)
+    async def cmd(self, cmd):
+        loop = asyncio.get_event_loop()
 
-        header = self.sock.recv(9)
+        if type(cmd) != str:
+            message = (cmd.decode() + '\n').encode()
+        else:
+            message = (cmd+'\n').encode()
+        loop.sock_sendall(self.sock, message)
+
+        header = await loop.sock_recv(self.sock, 9)
         current_len = len(header)
         while current_len < 9:
-            header += self.sock.recv(9-current_len)
+            header += await loop.sock_recv(self.sock, 9-current_len)
             current_len = len(header)
 
         success, bytes_to_read = struct.unpack('>?Q', header)
-        body = self.sock.recv(8)
+        if bytes_to_read == 0:
+            return success, ""
+
+        body = await loop.sock_recv(self.sock, 1)
         body_len = len(body)
         while body_len < bytes_to_read:
             len_to_read = bytes_to_read - body_len
             if len_to_read > 32:
                 len_to_read = 32
-            body += self.sock.recv(len_to_read)
+            body += await loop.sock_recv(self.sock, len_to_read)
             body_len = len(body)
         return success, body
 
     def destroy(self):
         self.sock.close()
 
-    def info(self):
-        return self.cmd("INFO")
+    async def info(self):
+        return await self.cmd("INFO")
 
-    def countall(self):
-        return self.cmd("COUNT ALL")
+    async def countall(self):
+        return await self.cmd("COUNT ALL")
 
-    def countall_in_mem(self):
-        return self.cmd("COUNT ALL IN MEM")
+    async def countall_in_mem(self):
+        return await self.cmd("COUNT ALL IN MEM")
 
-    def ping(self):
-        return self.cmd("PING")
+    async def ping(self):
+        return await self.cmd("PING")
     
-    def help(self):
-        return self.cmd("HELP")
+    async def help(self):
+        return await self.cmd("HELP")
 
-    def insert(self, ts, seq, is_trade, is_bid, price, size, dbname):
-        return self.cmd("INSERT {}, {}, {} ,{}, {}, {}; INTO {}"
+    async def insert(self, ts, seq, is_trade, is_bid, price, size, dbname):
+        return await self.cmd("INSERT {}, {}, {} ,{}, {}, {}; INTO {}"
                         .format( ts, seq, 
                             't' if is_trade else 'f',
                             't' if is_bid else 'f', price, size,
                             dbname))
 
-    def add(self, ts, seq, is_trade, is_bid, price, size):
-        return self.cmd("ADD {}, {}, {} ,{}, {}, {};"
+    async def add(self, ts, seq, is_trade, is_bid, price, size):
+        return await self.cmd("ADD {}, {}, {} ,{}, {}, {};"
                         .format( ts, seq, 
                             't' if is_trade else 'f',
                             't' if is_bid else 'f', price, size))
-    def bulkadd(self, updates):
-        self.cmd("BULKADD")
+    async def bulkadd(self, updates):
+        await self.cmd("BULKADD")
         for update in updates:
             ts, seq, is_trade, is_bid, price, size = update
 
-            self.cmd("{}, {}, {} ,{}, {}, {};"
+            await self.cmd("{}, {}, {} ,{}, {}, {};"
                     .format( ts, seq,
                             't' if is_trade else 'f', 
                             't' if is_bid else 'f', price, size))
-        self.cmd("DDAKLUB")
+        await self.cmd("DDAKLUB")
 
-    def getall(self):
-        return json.loads(self.cmd("GET ALL AS JSON"));
+    async def getall(self):
+        return json.loads(await self.cmd("GET ALL AS JSON"));
 
-    def get(self, n):
-        success, ret = self.cmd("GET {} AS JSON".format(n))
+    async def get(self, n):
+        success, ret = await self.cmd("GET {} AS JSON".format(n))
         if success:
             return json.loads(ret)
         else:
             return None
 
-    def clear(self):
-        return self.cmd("CLEAR")
+    async def clear(self):
+        return await self.cmd("CLEAR")
 
-    def clearall(self):
-        return self.cmd("CLEAR ALL")
+    async def clearall(self):
+        return await self.cmd("CLEAR ALL")
 
-    def flush(self):
-        return self.cmd("FLUSH")
+    async def flush(self):
+        return await self.cmd("FLUSH")
 
-    def flushall(self):
-        return self.cmd("FLUSH ALL")
+    async def flushall(self):
+        return await self.cmd("FLUSH ALL")
 
-    def create(self, dbname):
-        return self.cmd("CREATE {}".format(dbname))
+    async def create(self, dbname):
+        return await self.cmd("CREATE {}".format(dbname))
 
-    def use(self, dbname):
-        return self.cmd("USE {}".format(dbname))
+    async def use(self, dbname):
+        return await self.cmd("USE {}".format(dbname))
 
-    def unsubscribe(self):
-        self.cmd("UNSUBSCRIBE")
+    async def unsubscribe(self):
+        await self.cmd("UNSUBSCRIBE")
         self.subscribed = False
 
-    def subscribe(self, dbname):
-        res = self.cmd("SUBSCRIBE {}".format(dbname))
+    async def subscribe(self, dbname):
+        res = await self.cmd("SUBSCRIBE {}".format(dbname))
         if res[0]:
             self.subscribed = True
         return res
     
-    def poll(self):
-        return self.cmd("")
+    async def poll(self):
+        return await self.cmd("")
 
+    async def range(self, dbname, start, finish):
+        self.use(dbname)
+        data = await self.cmd("GET ALL FROM {} TO {} AS CSV".format(start, finish).encode())
+        data = data[1]
+        return data
 
+def measure_latency():
+    dts = []
+
+    db = TectonicDB()
+
+    t = time.time()
+    for i in range(10000):
+        db.insert(0,0,True, True, 0., 0., 'default')
+        t_ = time.time()
+        dt = t_ - t
+        t = t_
+        dts.append(dt)
+    print("AVG:", sum(dts) / len(dts))
+    db.destroy()
+
+def insert_n(n):
+    db = TectonicDB()
+    for i in range(n):
+        db.insert(0,0,True, True, 0., 0., 'default')
+
+def example_subscribe():
+    db = TectonicDB()
+    print(db.subscribe('default'))
+
+    import threading
+    def send_req():
+        while 1:
+            print("----------------------------")
+            insert_n(100)
+            time.sleep(3)
+    t = threading.Thread(target=send_req)
+    t.start()
+
+    while 1:
+        _, item = db.poll()
+        if item == "NONE\n":
+            time.sleep(0.01)
+        else:
+            print(item[0],)
