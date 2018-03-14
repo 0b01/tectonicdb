ad8999edaa88dc085a96935d8be5cdc7252b6077
ricky han <rickylqhan@gmail.com>
2018-03-13 22:11:39 -0400

    feature: add draw_updates function to draw candles

--- src/lib/postprocessing/candle/mod.rs
+++ src/lib/postprocessing/candle/mod.rs
@@ -1,3 +1,5 @@
+use dtf;
+
 pub mod candlestick_graph;
 pub mod candles;
 pub mod candle;
@@ -5,10 +7,12 @@ pub mod candle;
 pub use self::candles::Candles;
 pub use self::candle::Candle;
 
-use std::collections::{HashSet};
-
-
 type Time = u32;
 type Price = f32;
 type Volume = f32;
 type Scale = u16;
+
+pub fn draw_updates(ups: &[dtf::Update]) -> String {
+    let candles = Candles::from(ups);
+    candlestick_graph::CandleStickGraph::new(20, candles.clone()).draw()
+}
